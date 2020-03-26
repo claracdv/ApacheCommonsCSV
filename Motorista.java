7f7f86c4b97e7c44d2fce3eb73f0294e6122c4ef
Vicente <ivcente@gmail.com>
2020-03-26 02:16:51 -0300

    Classe e metodos Motorista definidos

--- Motorista.java
+++ Motorista.java
@@ -2,10 +2,31 @@
  * Motorista
  */
 public class Motorista {
-    public static void main(String[] args) {
-        //getCPF(): String
-        //getNome(): String
-        //getVeiculo(): Veiculo
-        //getFormaPgto(): FormaPagto[]
+    private String cpf;
+    private String nome;
+    private Veiculo veiculo;
+    private FormaPagamento fPagamento;
+
+    public Motorista(String cpf, String nome, Veiculo veiculo, FormaPagamento fPagamento){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.veiculo = veiculo;
+        this.fPagamento = fPagamento;
+    }
+
+    public String getCPF(){
+        return cpf;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+
+    public Veiculo getVeiculo(){
+        return veiculo;
+    }
+
+    public FormaPagamento getfPagamento(){
+        return fPagamento;
     }
 }
