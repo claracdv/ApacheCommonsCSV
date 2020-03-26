af66f077c331219dd388c2d10b8194a847e64d00
Vicente <ivcente@gmail.com>
2020-03-26 01:27:33 -0300

    Classe e metodos Passageiro definidos

--- Passageiro.java
+++ Passageiro.java
@@ -2,10 +2,31 @@
  * Passageiro
  */
 public class Passageiro {
-    public static void main(String[] args) {
-        //getCPF(): String
-        //getNome(): String
-        //getFormaPgto(): FormaPagto
-        //getNroCartao(): String
+    private String cpf;
+    private String nome;
+    private FormaPagamento formaPgto;
+    private String numeroCartao;
+
+    public Passageiro(String cpf, String nome, FormaPagamento formaPgto, String numeroCartao){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.formaPgto = formaPgto;
+        this.numeroCartao = numeroCartao;    
+    }
+    
+    public String getCPF(){
+        return cpf;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public FormaPagamento getFormaPgto(){
+        return formaPgto;
+    }
+
+    public String getNroCartao(){
+        return numeroCartao;
     }
 }
